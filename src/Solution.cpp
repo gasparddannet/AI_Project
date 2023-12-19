5129c743978a55cb869a293b6f52b7fe8e72f2e6
Arthur Houdant <arthur.houdant@alumni.enac.fr>
2023-12-19 11:35:43 +0100

    randomize

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -52,23 +52,12 @@ void Solution::randomize(vector<Operation> vectOperations)
     std::random_device rd;
     std::default_random_engine generator(rd());
 
-    for (int i = 0; i < solution.size()-1; i++)
+    for (int i = 0; i < vectOperations.size()-1; i++)
     {
-        vector<int> vectOp; 
-        vector<int> compParkings = vectOperations[i].getCompParkings();
-        // solution[i] = compParkings[rand() % compParkings.size()];
-        std::uniform_int_distribution<int> distribution1(0, 5);
+        std::uniform_int_distribution<int> distribution1(0, solution.size());
         int k = distribution1(generator);
-        for (int j=0; j<k; j++)
-        {
-            std::uniform_int_distribution<int> distribution2(0, vectOperations.size());
-            vectOp.push_back(distribution2(generator));
-        }
-        // std::cout << distribution(generator) << endl;
-        solution[i] = vectOp;
+        solution[k].push_back(i);
     }
-    vector<int> vectNonAlloc;
-    solution[solution.size()-1] = vectNonAlloc;
 }
 
 //////////////////////////////////////////////////// OPERATORS //////////////////////////////////////////////
